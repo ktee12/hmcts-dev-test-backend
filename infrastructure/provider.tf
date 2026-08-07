1d0e44c335254a2a2a3a85db2d317bc85c761f4c
Adekunle Aderibigbe <aderibigbe.adekunle21@gmail.com>
2026-08-07 23:19:13 +0100

    Separated terraform code from provider code

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_version = ">= 1.8.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
-
-  # In a real deployment, state would be stored remotely in Azure Storage.
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-hmcts-terraform-state"
-  #   storage_account_name = "sthmctsterraformstate"
-  #   container_name       = "tfstate"
-  #   key                  = "hmcts-dev-test/prod.tfstate"
-  # }
-}
-
 provider "azurerm" {
   features {}
 }
